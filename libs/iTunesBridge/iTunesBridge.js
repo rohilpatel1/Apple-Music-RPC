33c7ee1a679cff19c8b94e1de50c584bbc0546fd
theboys619 <theboys619@gmail.com>
2020-09-07 00:04:32 -0400

    Major fix to all builds. Issue with file pathing.

--- libs/iTunesBridge/iTunesBridge.js
+++ libs/iTunesBridge/iTunesBridge.js
@@ -1,5 +1,9 @@
 const { execSync, exec } = require("child_process");
+const Path = require("path");
 const os = require("os");
+
+const LIBPATHWS = Path.join(process.resourcesPath, "./app/libs/iTunesBridge/iTunesBridgeWS.js");
+const LIBPATHOS = Path.join(process.resourcesPath, "./app/libs/iTunesBridge/iTunesBridgeOA.js");
 
 class iTunes {
   constructor() {
@@ -12,9 +16,9 @@ class iTunes {
   setup() {
     try {
       if (os.platform() == "win32") {
-        this.currentSong = JSON.parse(execSync("cscript //Nologo ./libs/iTunesBridge/iTunesBridgeWS.js currentTrack", { encoding: "utf8" }));
+        this.currentSong = JSON.parse(execSync(`cscript //Nologo ${LIBPATHWS} currentTrack`, { encoding: "utf8" }));
       } else if (os.platform() == "darwin") {
-        this.currentSong = JSON.parse(execSync(`osascript ./libs/iTunesBridge/iTunesBridgeOA.js currentTrack`, { encoding: "utf8" }));
+        this.currentSong = JSON.parse(execSync(`osascript ${LIBPATHOS} currentTrack`, { encoding: "utf8" }));
       }
     } catch (e) {
       this.currentSong = {
@@ -25,17 +29,17 @@ class iTunes {
 
   exec(option) {
     if (os.platform() == "win32") {
-      return JSON.parse(execSync("cscript //Nologo ./libs/iTunesBridge/iTunesBridgeWS.js " + option, { encoding: "utf8" }));
+      return JSON.parse(execSync(`cscript //Nologo ${LIBPATHWS} ${option}`, { encoding: "utf8" }));
     } else if (os.platform() == "darwin") {
-      return JSON.parse(execSync(`osascript ./libs/iTunesBridge/iTunesBridgeOA.js ${option}`, { encoding: "utf8" }));
+      return JSON.parse(execSync(`osascript ${LIBPATHOS} ${option}`, { encoding: "utf8" }));
     }
   }
 
   getCurrentSong() {
     if (os.platform() == "win32") {
-      this.currentSong = JSON.parse(execSync("cscript //Nologo ./libs/iTunesBridge/iTunesBridgeWS.js currentTrack", { encoding: "utf8" }));
+      this.currentSong = JSON.parse(execSync(`cscript //Nologo ${LIBPATHWS} currentTrack`, { encoding: "utf8" }));
     } else if (os.platform() == "darwin") {
-      this.currentSong = JSON.parse(execSync(`osascript ./libs/iTunesBridge/iTunesBridgeOA.js currentTrack`, { encoding: "utf8" }));
+      this.currentSong = JSON.parse(execSync(`osascript ${LIBPATHOS} currentTrack`, { encoding: "utf8" }));
     }
     return this.currentSong;
   }
